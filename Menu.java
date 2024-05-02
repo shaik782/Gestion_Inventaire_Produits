2e6da06f56f33af72b0300d82afe8bda82ca1226
asiianlao <alexandrevlao@gmail.com>
2024-05-02 17:01:06 +0200

    modification menu et creation rajoutProduit

--- Menu.java
+++ Menu.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Menu {
-
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice;
@@ -10,42 +10,39 @@ public class Menu {
             System.out.println("Menu :");
             System.out.println("1. Rajout Voiture");
             System.out.println("2. Modifier Voiture");
-            System.out.println("4. Supprimer Voiture");
-            System.out.println("5. Importer Voiture");
-            System.out.println("6. Exporter Voiture");
-            System.out.println("7. Afficher Voiture");
-            System.out.println("8. Quitter");
+            System.out.println("3. Supprimer Voiture");
+            System.out.println("4. Importer Voiture");
+            System.out.println("5. Exporter Voiture");
+            System.out.println("6. Afficher Voiture");
+            System.out.println("7. Quitter");
             System.out.print("Entrez votre choix: ");
             choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-                    System.out.println("1. Rajout Voiture");
+                    System.out.println("Rajout Voiture");
                     break;
                 case 2:
-                    System.out.println("Vous avez choisi l'option 2.");
+                    System.out.println(" Modifier Voiture");
                     // Ajoutez le code de l'option 2 ici
                     break;
                 case 3:
-                    System.out.println("Vous avez choisi l'option 3.");
+                    System.out.println(" Supprimer Voiture");
                     // Ajoutez le code de l'option 3 ici
                     break;
                 case 4:
-                    System.out.println("Au revoir !");
+                    System.out.println("Importer Voiture");
                     break;
                 case 5:
-                    System.out.println("Au revoir !");
+                    System.out.println("Exporter Voiture");
                     break;
                 case 6:
-                    System.out.println("Vous avez choisi l'option 2.");
+                    System.out.println("Afficher Voiture.");
                     // Ajoutez le code de l'option 2 ici
                     break;
                 case 7:
-                    System.out.println("Vous avez choisi l'option 3.");
-                    // Ajoutez le code de l'option 3 ici
-                    break;
-                case 8:
                     System.out.println("Au revoir !");
+                    // Ajoutez le code de l'option 3 ici
                     break;
                 default:
                     System.out.println("Choix invalide. Veuillez réessayer.");
--- RajoutProduit.java
+++ RajoutProduit.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
-public class RajoutProduit {
-
-    public static void main(String[] args){
+public class RajoutProduit extends MainConnection {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Rajout d'une voiture:");
         System.out.println("Donner le nom de la marque");
@@ -14,13 +11,18 @@ public class RajoutProduit {
         System.out.println("Donner la couleur de la voiture");
         String couleur = scanner.nextLine();
         System.out.println("Donner l'année de production de la voiture");
-        String couleur1 = scanner.nextLine();
+        String annee = scanner.nextInt();
         System.out.println("Donner le prix unitaire de la voiture");
+        String prix = scanner.nextFloat();
         System.out.println("Donner la quantité de cette voiture");
+        String quantite = scanner.nextInt();
         System.out.println("Donner le nombre de place dans cette voiture");
+        String nbplace = scanner.nextInt();
         System.out.println("8. Quitter");
         System.out.print("Entrez votre choix: ");
-        return;
+        Statement requete = connexion.createStatement();
+        ResultSet listeProduits = requete.executeQuery("SELECT * FROM produit");
+        return 
     }
 
 }
